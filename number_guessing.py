de394223299aa2c76984f2795556c74e9ee072f4
Aashish1023 <aashishmaharjan1999@gmail.com>
2025-06-25 16:19:50 +0545

    Implement number guessing game logic

--- number_guessing.py
+++ number_guessing.py
@@ -1,1 +1,21 @@
 import random
+
+# 1: Generate a Random Number
+secret_number = random.randint(1, 100)
+attempts = 0
+
+print("Welcome to the Number Guessing Game! ")
+print("I'm thinking of a number between 1 and 100.")
+
+# 2: Loop Until user guesses the number
+while True:
+    guess = int(input("Take a guess: "))
+    attempts += 1   
+
+    if guess < secret_number:
+        print("Your guess is too low. Try again!")
+    elif guess > secret_number:
+        print("Your guess is too high. Try again!")
+    else:
+        print(f"Congratulations! You've guessed the number {secret_number} in {attempts} attempts.")
+        break
