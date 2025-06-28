c192c9d23741db2743158255736538f13bec67d4
Aashish1023 <aashishmaharjan1999@gmail.com>
2025-06-28 22:17:24 +0545

    Refactor menu display and task management functions in todo list application

--- todo_list.py
+++ todo_list.py
@@ -3,8 +3,21 @@
 todo_list = []
 
 def show_menu():
-    print("\nTodo List Application")
-    print("1. Add a Todo")
-    print("2. View Todos")
-    print("3. Remove a Todo")
+    print(" To-Do List Application")
+    print("1. Add a Task")
+    print("2. View Tasks")
+    print("3. Remove a Task")
     print("4. Exit")
+
+def add_task():
+    task = input("Enter the task you want to add: ")
+    todo_list.append(task)
+    print(f"Task '{task}' added successfully!")
+
+def view_tasks():
+    if not todo_list:
+        print("No tasks in the list.")
+    else:
+        print("Your To-Do List: ")
+        for index, task in enumerate(todo_list, start=1):
+            print(f"{index}. {task}")
