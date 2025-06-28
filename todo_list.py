59e181cf636945b9b07a2341da6470c67c2d89aa
Aashish1023 <aashishmaharjan1999@gmail.com>
2025-06-28 22:09:24 +0545

    Add menu display for todo list application

--- todo_list.py
+++ todo_list.py
@@ -1,3 +1,10 @@
 #Todo list application
 
 todo_list = []
+
+def show_menu():
+    print("\nTodo List Application")
+    print("1. Add a Todo")
+    print("2. View Todos")
+    print("3. Remove a Todo")
+    print("4. Exit")
