ecc10d45b71396f19d1a31ac38df7647e1d13636
Aashish1023 <aashishmaharjan1999@gmail.com>
2025-07-06 16:12:58 +0545

    Add main window setup and label for digital clock application

--- digital_clock.py
+++ digital_clock.py
@@ -1,2 +1,14 @@
 import tkinter as tk
 from time import strftime
+
+#Create the main window
+root = tk.Tk()
+root.title("Digital clock")
+
+# Set the background color
+root.geometry("400x200")
+root.configure(bg="black")
+
+# Create a label to display the time
+label = tk.Label(root, font=("calibri", 40, "bold"), bg="black", fg="white")
+label.pack(anchor='center', pady=20)
